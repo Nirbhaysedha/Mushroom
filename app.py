68a4b1fbd587a66ad94eb5063303ab574ee56480
NirbhaySedha <Sedha9nirbhay@gmail.com>
2024-04-19 08:18:21 +0530

    up

---
+++ app.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from joblib import load
+from pydantic import BaseModel
+import pandas as pd
+
+class PredictionInput(BaseModel):
+    cap_diameter: int
+    cap_shape: int
+    gill_attachment: int
+    gill_color: int
+    stem_height: float
+    stem_width: int
+    stem_color: int
+    season: float
+
+
+app=FastAPI()
+model_path='models/model.joblib'
+model=load(model_path)
+
+@app.get('/home')
+def home():
+    return f"hey we are on successfull!"
+
+@app.post('/predict')
+def predict(input_data: PredictionInput):
+        features={
+    "cap-diameter":input_data.cap_diameter,
+    "cap-shape":input_data.cap_shape,
+    "gill-attachment":input_data.gill_attachment,
+    "gill-color":input_data.gill_color,
+    "stem-height":input_data.stem_height,
+    "stem-width":input_data.stem_width,
+    "stem-color":input_data.stem_color,
+    "season":input_data.season
+        }
+        feat=pd.DataFrame(features,index=[0])
+        prediction = model.predict(feat)[0].item()
+        return f"predictions : {prediction}"
+
+if __name__=="__main__":
+     import uvicorn
+     uvicorn.run(app,host="0.0.0.0",port=8000)
+
---
+++ src/models/predict_model.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import joblib
+import pathlib
+
+def load_data(data_path,target,testseed,seed):
+    df=pd.read_csv(data_path)
+    x=df.drop(columns=target)
+    y=df[target]
+    x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=testseed,random_state=seed)
+    return x_train,x_test,y_train,y_test
+
+
+def model_building(x_train, x_test, y_train, y_test, path):
+    model = LogisticRegression()
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    try:
+        joblib.dump(model, path + "/model.joblib")
+    except Exception as e:
+        return f"Unable to dump model because of {e}"
+
+
+def main():
+    curr_dir=pathlib.Path(__file__)
+    parent_dir=curr_dir.parent.parent.parent
+    data_path=parent_dir.as_posix()+'/data/raw/mushroom_cleaned.csv'
+    target='class'
+    testseed=0.2
+    seed=42
+    x_train,x_test,y_train,y_test=load_data(data_path,target,testseed,seed)
+    path=parent_dir.as_posix()+'/models'
+    model_building(x_train,x_test,y_train,y_test,path)
+    
+
+if __name__=='__main__':
+    main()
